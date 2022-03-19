f462689ff2d41b13418da02f33a7b6166f368004
Marcelo Santos <rocha.mer21@gmail.com>
2022-03-19 14:55:41 -0300

    criado o card e add radius corner

--- app/src/main/java/xyz/com/marcelo/bizcard/MainActivity.kt
+++ app/src/main/java/xyz/com/marcelo/bizcard/MainActivity.kt
@@ -3,13 +3,16 @@ package xyz.com.marcelo.bizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import xyz.com.marcelo.bizcard.ui.theme.BizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,9 +33,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
-    Surface(modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight()) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            elevation = 4.dp
+        ) {
+
+        }
 
     }
 }
